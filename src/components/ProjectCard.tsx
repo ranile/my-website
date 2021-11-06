4d091448d86dd4f8af88b87889dfe83abf3e7f13
Hamza <muhammadhamza1311@gmail.com>
2021-11-06 14:14:12 +0500

    extract projects list out to it's own component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -5,25 +5,30 @@ const Language = ({children}: PropsWithChildren<{}>) => {
     return <span className="text-sm">{children}</span>
 }
 
-export default function ProjectCard(props: { project: string }) {
+interface Props { 
+    title: string
+    link: string
+    description: string,
+    languages: string[]
+}
+
+export default function ProjectCard(props: Props) {
     return (
         <article className="flex flex-col shadow-lg items-center h-56 max-w-sm px-5  gap-5
             hover:scale-105 transition duration-300 ease-in-out">
             <h3 className="text-xl font-bold pt-4">
-                {props.project}
+                {props.title}
             </h3>
 
             <p className="text-center">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo enim asperiores
+                {props.description}
             </p>
 
             <section className="flex gap-4">
-                <Language>Rust</Language>
-                <Language>HTML</Language>
-                <Language>SCSS</Language>
+                {props.languages.map(lang => <Language>{lang}</Language>)}
             </section>
 
-            <AnimatedBottomBorderLink href={`https://github.com/hamza1311/${props.project}`}  className="pb-7 hover:before:w-6">
+            <AnimatedBottomBorderLink href={`https://github.com/${props.link}`}  className="pb-7 hover:before:w-6">
                 <img style={{width: 24, height: 24}} src="/icons/GitHub-Dark.png" />
             </AnimatedBottomBorderLink>
 
