224160c00bb2b521b5e6656199c36428f03ac1eb
Hamza <muhammadhamza1311@gmail.com>
2021-11-07 16:59:25 +0500

    head

--- src/components/AnimatedBottomBorderLink.tsx
+++ src/components/AnimatedBottomBorderLink.tsx
@@ -1,8 +1,9 @@
 import { PropsWithChildren } from 'react'
 
 const AnimatedBottomBorderLink = ({children, href, pb = '2', className = '', target = ''}:  PropsWithChildren<{ href: string, pb: string, className?: string, target?: string }>) => {
+    const rel = target === '_blank' ? "noopener" : ''
     return (
-        <a href={href} target={target} className={`
+        <a href={href} target={target} rel={rel} className={`
             relative pb-${pb}
             hover:before:w-full
             before:inline before:bg-pink-500 before:w-0 before:absolute before:h-0.5 before:bottom-0
