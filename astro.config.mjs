ba8dd88388c2cb5d1fb98ec7d03ef7a54d14424d
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-06-06 01:56:06 +0500

    update deps + clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 
+// https://astro.build/config
 export default defineConfig({
-    site: "https://mhamza.dev",
+    site: "https://elina.website/",
     integrations: [
         react(),
         tailwind({
             config: {
                 path: "./tailwind.config.cjs",
+                applyBaseStyles: false,
             },
         }),
     ],
